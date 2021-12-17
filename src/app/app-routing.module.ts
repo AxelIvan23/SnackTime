f18f3369dc81db5ebeecc33358c6c3b815fd5bea
Lupita Gallegos Rdz <Windows@DESKTOP-RPUHS8P.proyectoredes.com>
2021-12-17 14:37:10 -0600

    parte lupita

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { from } from 'rxjs';
 import { FirstComponent} from './first/first.component';
@@ -7,6 +7,8 @@ import { TerceroComponent} from './tercero/tercero.component';
 import { FourtComponent} from './fourt/fourt.component';
 import { SixcomponentComponent } from './sixcomponent/sixcomponent.component';
 import { FiveComponent} from './five/five.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { IniciarsesionComponent } from './iniciarsesion/iniciarsesion.component';
 
 
 
@@ -16,7 +18,9 @@ const routes: Routes = [
   { path: 'tercero-component', component: TerceroComponent},
   { path: 'fourt-component', component: FourtComponent},
   { path: 'iniciar-sesion',component: FiveComponent},
-  { path: 'six-component', component: SixcomponentComponent}
+  { path: 'six-component', component: SixcomponentComponent},
+  { path: 'perfil-Empresa', component: HeroDetailComponent},
+  { path: 'registrar-Restaurante', component: IniciarsesionComponent}
 ];
 
 @NgModule({
