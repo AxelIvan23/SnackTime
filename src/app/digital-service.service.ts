c1067c8ec02cc3246fc3b9f649e9329ccf80ad98
AxelIvan23 <68132702+AxelIvan23@users.noreply.github.com>
2021-12-17 20:26:32 -0600

    proyecto merged

--- src/app/digital-service.service.ts
+++ src/app/digital-service.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DigitalServiceService {
 
-  constructor() { }
+   url: string = "http://localhost:4201";
+
+   constructor(private httpClient: HttpClient) { }
+
+   get(query : string): Observable<any[]> {
+       return this.httpClient.get(this.url+"/"+query) as Observable<any[]>;
+   }
 }
